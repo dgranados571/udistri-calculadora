9bf527a1581e52de4e7d434f1d886626dbe938fe
Danny Granados <dgranados571@gmail.com>
2023-12-04 18:46:20 -0500

    Commit 1 entrega Actividad

---
+++ src/test/java/com/co/calculadora/CalculadoraUdistriAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraUdistriAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
